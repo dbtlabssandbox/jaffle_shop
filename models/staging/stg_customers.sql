4828488eddc466cf1747d1afe5a7100e8edf6fac
dev@dbtlabs.com <dev@dbtlabs.com>
2025-03-31 22:28:37 +0000

    test

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -2,7 +2,7 @@ with source as (
 
     {#-
     Normally we would select from the table here, but we are using seeds to load
-    our data in this project
+    our data in this project 2
     #}
     select * from {{ ref('raw_customers') }}
 
